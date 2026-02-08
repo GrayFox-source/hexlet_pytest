5314359b5bc8023499480f67cee178c6197adcb6
flexabler <smolnikov2023@list.ru>
2026-02-08 14:07:30 +0500

    create test_stack;

---
+++ tests/test_stack.py
@@ -0,0 +1,23 @@
+import pytest
+
+def test_stack():
+    stack = []
+    stack.append(1)
+    stack.append(2)
+
+    assert stack.pop() == 2
+    assert stack.pop() == 1
+
+def test_stack_emptyness():
+    stack = []
+    assert not stack
+    stack.append(1)
+    assert bool(stack)
+    stack.pop()
+    assert not stack
+
+def test_pop_with_empty_stack():
+    stack = []
+    with pytest.raises(IndexError):
+        stack.pop()
+
